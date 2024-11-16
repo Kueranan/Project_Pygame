1499d2d6716f2687d71bea702d8db895b664739b
Alvphon <thanakritcho@gmail.com>
2024-11-16 12:48:05 +0700

    modify pos button.py

--- button.py
+++ button.py
@@ -2,20 +2,23 @@ import pygame
 from pygame.locals import *
 
 class Button:
-    def __init__(self, screen, pos, up_image, down_image):
+    def __init__(self, screen, pos, up_image, down_image, size = None):
         self.screen = screen
         self.image_up = pygame.image.load(up_image).convert_alpha()
         self.image_down = pygame.image.load(down_image).convert_alpha()
+        
+        if size:
+            self.image_up = pygame.transform.scale(self.image_up, size)
+            self.image_down = pygame.transform.scale(self.image_down, size)
+        
         self.image = self.image_up
         self.rect = self.image.get_rect(center=pos)
         self.clicked = False
 
     def draw(self):
-        # Draw the button on the screen
         self.screen.blit(self.image, self.rect)
 
     def is_clicked(self):
-        # Check if button is clicked
         mouse_pos = pygame.mouse.get_pos()
         mouse_pressed = pygame.mouse.get_pressed()
         
