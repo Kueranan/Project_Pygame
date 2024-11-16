63d8e267863aa7ab6a52f8a6e235bc2d5640f40f
Alvphon <thanakritcho@gmail.com>
2024-11-16 12:53:26 +0700

    ...

--- result02.py
+++ result02.py
@@ -18,15 +18,16 @@ def result():
 
     background = pygame.image.load('images/night.jpeg').convert_alpha()
     background =  pygame.transform.scale(background, (WIDTH, HEIGHT))
-    rect_surface = pygame.Surface((720, 720), pygame.SRCALPHA)  
-    rect_color = (102, 102, 102, 150)
-    pygame.draw.rect(rect_surface, rect_color, pygame.Rect(0, 0, 620, 620), border_radius=10)
     grade = pygame.image.load('images/A.jpeg').convert_alpha()
     grade =  pygame.transform.scale(grade, (240, 240))
 
-    choice_size = (250, 80)
-    oRetry = Button(screen, (WIDTH//2, HEIGHT*6.5//10), 'images/retry.png', 'images/retry_down.png', size = choice_size)
-    oExit = Button(screen, (WIDTH//2, HEIGHT*8//10), 'images/exit.png', 'images/exit_down.png', size = choice_size)
+    rect_surface = pygame.Surface((720, 720), pygame.SRCALPHA)  
+    rect_color = (102, 102, 102, 150)
+    pygame.draw.rect(rect_surface, rect_color, pygame.Rect(0, 0, 620, 620), border_radius=10)
+    
+
+    oRetry = Button(screen, (WIDTH//2, HEIGHT*6.5//10), 'images/retry.png', 'images/retry_down.png')
+    oExit = Button(screen, (WIDTH//2, HEIGHT*8//10), 'images/exit.png', 'images/exit_down.png')
 
     # Game loop
     run = True
@@ -60,4 +61,3 @@ result()
 
 pygame.quit()
 sys.exit()
-
