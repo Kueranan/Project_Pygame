a090eea31d30abea93fffaeb0477b6789604a453
Alvphon <thanakritcho@gmail.com>
2024-11-16 12:57:00 +0700

    modify mode_button.py

--- mode_button.py
+++ mode_button.py
@@ -1,29 +1,30 @@
 import pygame
 
 class RadioButton(pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h, image_path):
+    def __init__(self, x, y, w, h, image_path,):
         super().__init__() 
+        self.border_radius = 15
         image = pygame.image.load(image_path).convert_alpha()
         image = pygame.transform.scale(image, (w, h))
         
         self.button_image = pygame.Surface((w, h), pygame.SRCALPHA)
-        self.button_image.fill((96, 96, 96, 255))
+        self.button_image.fill((0, 0, 0, 0))
         self.button_image.blit(image, image.get_rect(center=(w // 2, h // 2)))
 
         self.hover_image = pygame.Surface((w, h), pygame.SRCALPHA)
-        self.hover_image.fill((96, 96, 96, 255))
+        self.hover_image.fill((0, 0, 0, 0))
         self.hover_image.blit(image, image.get_rect(center=(w // 2, h // 2)))
-        pygame.draw.rect(self.hover_image, (0, 153, 219), self.hover_image.get_rect(), 3)
+        pygame.draw.rect(self.hover_image, (0, 153, 219), self.hover_image.get_rect(), 3, border_radius=self.border_radius)
 
         self.clicked_image = pygame.Surface((w, h), pygame.SRCALPHA)
-        self.clicked_image.fill((96, 196, 96, 255))
+        self.clicked_image.fill((0, 0, 0, 0))
         self.clicked_image.blit(image, image.get_rect(center=(w // 2, h // 2)))
 
         self.image = self.button_image
-        self.rect = pygame.Rect(x, y, w, h)
+        self.rect = self.button_image.get_rect(center = (x,y))
+
         self.clicked = False
         self.buttons = None
-
         self.flash_timer = 0  
         self.flash_on = False
 
@@ -53,7 +54,7 @@ class RadioButton(pygame.sprite.Sprite):
 
             if self.flash_on:
                 self.image = self.clicked_image.copy()
-                pygame.draw.rect(self.image, (254, 174, 52), self.image.get_rect(), 3)
+                pygame.draw.rect(self.image, (254, 174, 52), self.image.get_rect(), 3, border_radius=self.border_radius)
             else:
                 self.image = self.clicked_image
 
